57cd72ac8ddc18271714c45890e11278704d7f9f
Alejandro Arellano <jesusarellano334@gmail.com>
2026-01-09 15:27:15 -0600

    chore: add in-memory posts model

---
+++ src/models/posts.js
@@ -0,0 +1,18 @@
+const posts = [
+  {
+    id: 1,
+    title: "Primer post",
+    content: "Contenido del primer post",
+    author: "Admin",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Segundo post",
+    content: "Contenido del segundo post",
+    author: "Admin",
+    date: new Date(),
+  },
+];
+
+module.exports = posts;
